f8daf65aeb95f1d7eeb2a224727e0b70215492a9
Huqo7 <vegfor07@gmail.com>
2024-09-30 13:07:18 +0200

    Test

--- parameter_estimation/main.cpp
+++ parameter_estimation/main.cpp
@@ -8,5 +8,10 @@ Eigen::MatrixXd pseudo_inverse(const Eigen::MatrixXd &A)
 
 int main()
 {
+    rapidcsv::Document doc("data/1-baseline_accel.csv");
+
+    std::vector<long> col = doc.GetColumn<long>("t");
+    std::cout << "Read " << col.size() << " values." << std::endl;
+
     return 0;
 }
